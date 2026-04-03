428d646a9221430e2c5182084bab642e70fe61e3
monsef benhar <mouncifbenhar7@gmail.com>
2026-04-03 09:55:19 +0100

    add app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Header from "./Header.jsx"
+import "./App.css"
+import  TaskInput from "./TaskInput.jsx"
+function App() {
+  return (
+       <>
+        <Header/>
+        <TaskInput/>
+       </>
+  )
+}
+
+export default App
