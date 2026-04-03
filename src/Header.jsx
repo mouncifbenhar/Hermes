03aabe5dc222b11fafa24a7cbaf0f9b20439866e
monsef benhar <mouncifbenhar7@gmail.com>
2026-04-03 09:54:39 +0100

    add header

---
+++ src/Header.jsx
@@ -0,0 +1,26 @@
+import Logo from './assets/Logo.png'
+
+export default function Header(){
+    return(
+ <section style={{
+  height: "70px",
+  backgroundColor: "#daefff",
+  padding: "0 20px",
+  display: "flex",
+  alignItems: "center"
+}}>
+  
+
+  <h1 style={{
+    marginLeft: "10px",
+    color: "#3a8dde",
+    fontSize: "24px",
+    marginTop: 0,
+    marginBottom: 0
+  }}>
+    Hermes
+  </h1>
+
+</section>
+)
+}
